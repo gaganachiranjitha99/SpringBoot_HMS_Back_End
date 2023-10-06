10badf30f4d3ddbb07855d36977d1fb1debf782b
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-10-06 13:53:27 +0530

    back end based

---
+++ HMS_BACKEND/src/main/java/com/rhms/hms_backend/HmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rhms.hms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsBackendApplication.class, args);
+	}
+
+}
---
+++ HMS_BACKEND/src/test/java/com/rhms/hms_backend/HmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhms.hms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
