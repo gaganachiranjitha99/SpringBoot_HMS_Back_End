1e21d75f29fcc29d696c3e4ba145cc423b5b4f13
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-10-12 00:46:30 +0530

    update user controller

--- HMS_BACKEND/src/main/java/com/rhms/hms_backend/Controllers/UserController.java
+++ HMS_BACKEND/src/main/java/com/rhms/hms_backend/Controllers/UserController.java
@@ -1,12 +1,12 @@
 package com.rhms.hms_backend.Controllers;
 
+import com.rhms.hms_backend.Models.Users;
 import com.rhms.hms_backend.Repositories.UserRepo;
 import com.rhms.hms_backend.Services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
+    @Autowired
     private final UserRepo userRepo;
 
     @Autowired
@@ -30,7 +31,7 @@ public class UserController {
     }
 
     @GetMapping("/allUsers")
-    public List<User> getAllUsers(){
+    public List<Users> getAllUsers(){
         return userRepo.findAll();
     }
 
