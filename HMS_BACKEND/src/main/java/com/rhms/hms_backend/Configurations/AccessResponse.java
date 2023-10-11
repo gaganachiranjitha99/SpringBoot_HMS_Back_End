96f39804b799eb52313318b60d576511e39adaad
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-10-11 22:56:13 +0530

    access response

---
+++ HMS_BACKEND/src/main/java/com/rhms/hms_backend/Configurations/AccessResponse.java
@@ -0,0 +1,15 @@
+package com.rhms.hms_backend.Configurations;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AccessResponse {
+    private String Token;
+}
