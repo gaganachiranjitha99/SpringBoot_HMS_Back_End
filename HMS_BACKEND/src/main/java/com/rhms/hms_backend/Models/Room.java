6a945666efd6b7a363322d2332e2f9532563acd3
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-25 09:27:39 +0530

    Create Room.java

---
+++ HMS_BACKEND/src/main/java/com/rhms/hms_backend/Models/Room.java
@@ -0,0 +1,20 @@
+package com.rhms.hms_backend.Models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "Room")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String roomID;
+    private String hostalType;
+}
