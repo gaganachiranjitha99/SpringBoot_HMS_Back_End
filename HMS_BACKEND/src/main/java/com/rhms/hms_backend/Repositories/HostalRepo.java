947edd617a49102370f18310b6c7423f03f76dab
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-25 17:36:24 +0530

    Create HostalRepo.java

---
+++ HMS_BACKEND/src/main/java/com/rhms/hms_backend/Repositories/HostalRepo.java
@@ -0,0 +1,9 @@
+package com.rhms.hms_backend.Repositories;
+
+import com.rhms.hms_backend.Models.Hostals;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HostalRepo extends JpaRepository<Hostals, String> {
+}
