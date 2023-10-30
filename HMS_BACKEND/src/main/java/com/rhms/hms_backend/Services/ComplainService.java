228261bf5f3d0d93c084ef7908d4db9a4b0cb5ee
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-30 15:39:51 +0530

    get complain by id

--- HMS_BACKEND/src/main/java/com/rhms/hms_backend/Services/ComplainService.java
+++ HMS_BACKEND/src/main/java/com/rhms/hms_backend/Services/ComplainService.java
@@ -2,12 +2,14 @@ package com.rhms.hms_backend.Services;
 
 import com.rhms.hms_backend.Models.Complain;
 import com.rhms.hms_backend.Repositories.ComplainRepo;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.awt.*;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ComplainService {
@@ -20,6 +22,16 @@ public class ComplainService {
         return complainRepo.save(complain);
     }
 
+
+    public Complain getById(Long id){
+        Optional<Complain> optionalComplaint = complainRepo.findById(id);
+
+        if (optionalComplaint.isPresent()) {
+            return optionalComplaint.get();
+        } else {
+            throw new EntityNotFoundException("Complaint with ID " + id + " not found");
+        }
+    }
 
 
     public java.util.List<Complain> getAllComplaints() {
