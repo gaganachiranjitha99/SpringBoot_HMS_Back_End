905256a5f2729f2f2736bc8cb556f37965ab04bf
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-10-30 18:59:47 +0530

    complain  update controller

--- HMS_BACKEND/src/main/java/com/rhms/hms_backend/Controllers/ComplainController.java
+++ HMS_BACKEND/src/main/java/com/rhms/hms_backend/Controllers/ComplainController.java
@@ -3,6 +3,7 @@ package com.rhms.hms_backend.Controllers;
 
 import com.rhms.hms_backend.Models.Complain;
 import com.rhms.hms_backend.Services.ComplainService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,20 @@ public class ComplainController {
         Complain createdComplain = complainService.createComplain(complain);
         return new ResponseEntity<>(createdComplain, HttpStatus.CREATED);
     }
+
+    @PutMapping("/updateComplain/{complainId}")
+    public Long updateComplain(@PathVariable("complainId") Long complainId, @RequestBody Complain updatedComplain) {
+        Complain existingComplaint = complainService.getById(complainId);
+
+        if (existingComplaint != null) {
+            updatedComplain.setId(complainId);
+            complainService.Save(updatedComplain);
+            return updatedComplain.getId();
+        } else {
+            throw new EntityNotFoundException("Complaint with ID " + complainId + " not found");
+        }
+    }
+
 
     @GetMapping("/getAllComplains")
     private List<Complain> getAllComplains(){
@@ -47,4 +62,4 @@ public class ComplainController {
     
 
 
-}
+
