0ee66d5bd7fb513673806585c602937f7c206a04
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-11-04 11:21:51 +0530

    get from subwarden repo

---
+++ HMS_BACKEND/src/main/java/com/rhms/hms_backend/Repositories/SubwardenRepo.java
@@ -0,0 +1,19 @@
+package com.rhms.hms_backend.Repositories;
+
+import com.rhms.hms_backend.Models.StaffUsers;
+import com.rhms.hms_backend.Models.SubwardenComplains;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Repository
+public interface SubwardenRepo extends JpaRepository<SubwardenComplains,Long> {
+
+    @Transactional
+    @Procedure(procedureName="get_Complain_From_Subwarden")
+    List<SubwardenComplains> get_Complain_From_Subwarden();
+
+
+}
